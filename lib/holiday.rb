1789aeaabb02b5ec2a0643035308663c3a52ff86
Nick Lanese <nlanese21@gmail.com>
2020-07-21 22:41:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,6 +94,7 @@ def all_supplies_in_holidays(holiday_hash)
       holidayNameStr = holidayName.to_s
       holidayNameStr = holidayNameStr.capitalize
       if (holidayNameStr.include?("_"))
+        nextPut += "  "
         holidayNameArray = holidayNameStr.split("_")
         maxHDNindex = holidayNameArray.length - 1
         i = 0
